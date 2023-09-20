708f23884fbf187fd13455ab5c33e7f4c88d2f8e
Alexandra4373 <77993156+Alexandra4373@users.noreply.github.com>
2023-09-20 22:05:48 +0000

    update

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -2,10 +2,10 @@ import { arrowUp } from "../assets";
 
 const GetStarted = () => {
   return (
-    <div className="flex justify-center items-center bg-blue-gradient w-[100px] h-[100px] rounded-full bg-blue p-[2px] cursor-pointer">
+    <div className="flex justify-center items-center border-blue-gradient border-2 w-[100px] h-[100px] rounded-full bg-blue p-[2px] cursor-pointer">
       <div className="flex flex-col justify-center items-center bg-primary w-[100%] h-[100%] rounded-full text-center">
         <div className="flex items-center">
-          <p className="font-poppins font-medium text-[18px] leading-[23.4px] text-gradient">
+          <p className="font-poppins font-medium text-[17px] leading-[23.4px] text-gradient">
             Get
           </p>
           <img
@@ -14,7 +14,7 @@ const GetStarted = () => {
             className="w-[23px] h-[23px] object-contain mx-1"
           />
         </div>
-        <p className="font-poppins font-medium text-[18px] leading-[23.4px] text-gradient">
+        <p className="font-poppins font-medium text-[17px] leading-[23.4px] text-gradient">
           Started
         </p>
       </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import GetStarted from "./GetStarted";
 
 const Hero = () => {
   return (
-    <div className="bg-slate-800 flex md:flex-row flex-col sm:py-16 py-6">
+    <div className="bg-slate-800 flex md:flex-col sm:py-16 pl-12">
       <div className="flex-1  justify-center items-start flex-col  xl:px-0 sm:px-16 px-6">
         <div className="flex  flex-row items-center bg-black py-[6px] px-4 rounded-[10px] mb-2 w-3/4">
           <img src={discount} alt="discount" className="[32px] h-[32px]" />
@@ -14,34 +14,27 @@ const Hero = () => {
           </p>
         </div>
 
-        <div className=" flex flex-row justify-between items-center w-full">
+        <div className=" flex justify-between items-center w-full">
           <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px">
             The Next <br className="sm:block hidden" />
             <span className="text-gradient ">Generation</span>
           </h1>
-          <div className="ss:flex hidden md:mr-4 mr-0">
+          {/* <div className="ss:flex md:mr-4 mr-0">
             <GetStarted />
-          </div>
-          <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
+          </div> */}
+          <h1 className=" flex-1 font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
             Payment Method.
           </h1>
-          <p className=" font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px] max-w-[470px] mt-5">
+          <p className="flex-1">
             Our team of experts uses a methodology to identify the credit cards
             most likely to fit your needs. We examine annual percentage rates,
             annual fees.
           </p>
         </div>
       </div>
-      <div className="flex-1 flex justify-center items-center  md:my-0 my-10 relative ">
-        <img
-          src={robot}
-          alt="billing"
-          className="w-[80%] h-[80%] relative z-[5]"
-        />
-      </div>
-      <div className="ss:hidden flex justify-center items-center ">
-        <GetStarted />
-      </div>
+      {/* <div className="flex-1 flex justify-center items-center  md:my-0 my-10 relative ">
+        <img src={robot} alt="billing" className="w-[80%] h-[80%] " />
+      </div> */}
     </div>
   );
 };
