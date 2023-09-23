ca4262b866f6677bf9dae5cd5de66db7616372a7
Alexandra4373 <77993156+Alexandra4373@users.noreply.github.com>
2023-09-23 10:06:51 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
       <nav className="bg-slate-800 w-full flex py-6 justify-between items-center navbar pl-10">
         <img src={logo} alt="hoobank" className="w-32 h-8]" />
 
-        <ul className=" text-white list-none sm:flex hidden justify-end items-center flex-1 gap-10 px-40 cursor-pointer  ">
+        <ul className=" text-white list-none sm:flex hidden justify-end items-center flex-1 gap-10 px-40 cursor-pointer ">
           <li>
             <a href="#Home">Home</a>
           </li>
@@ -27,29 +27,25 @@ function App() {
           <div className="flex  flex-row items-center bg-black py-[6px] px-4 rounded-[10px] mb-2 w-3/4">
             <img src={discount} alt="discount" className="[32px] h-[32px]" />
 
-            <p className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]  ml-2">
+            <p className="font-poppins font-normal text-white text-[18px] leading-[30.8px]  ml-2">
               <span className="text-white">20%</span> Discount for{" "}
               <span className="text-white">1 Month</span> Account
             </p>
           </div>
 
-          <div className=" flex justify-between items-center w-full">
-            <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px">
-              The Next <br className="sm:block hidden" />
-              <span className="text-gradient ">Generation</span>
-            </h1>
-            {/* <div className="ss:flex md:mr-4 mr-0">
-            <GetStarted />
-          </div> */}
-            <h1 className=" flex-1 font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
-              Payment Method.
-            </h1>
-            <p className="flex-1">
-              Our team of experts uses a methodology to identify the credit
-              cards most likely to fit your needs. We examine annual percentage
-              rates, annual fees.
-            </p>
-          </div>
+          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px">
+            The Next <br className="sm:block hidden" />
+            <span className="text-gradient ">Generation</span>
+          </h1>
+
+          <h1 className=" flex-1 font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
+            Payment Method.
+          </h1>
+          <p className="flex-1 w-60 text-[18px] text-white">
+            Our team of experts uses a methodology to identify the credit cards
+            most likely to fit your needs. We examine annual percentage rates,
+            annual fees.
+          </p>
         </div>
         {/* <div className="flex-1 flex justify-center items-center  md:my-0 my-10 relative ">
         <img src={robot} alt="billing" className="w-[80%] h-[80%] " />
