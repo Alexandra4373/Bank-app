3613c6d571f6d62e3ec281950e1f40534ce18326
Alexandra4373 <77993156+Alexandra4373@users.noreply.github.com>
2023-09-23 10:11:10 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,16 +24,16 @@ function App() {
 
       <div className="bg-slate-800 flex md:flex-col sm:py-16 pl-12">
         <div className="flex-1  justify-center items-start flex-col  xl:px-0 sm:px-16 px-6">
-          <div className="flex  flex-row items-center bg-black py-[6px] px-4 rounded-[10px] mb-2 w-3/4">
+          <div className="flex  flex-row items-center bg-black py-[6px] px-4 rounded-[10px] mb-2 w-1/4">
             <img src={discount} alt="discount" className="[32px] h-[32px]" />
 
-            <p className="font-poppins font-normal text-white text-[18px] leading-[30.8px]  ml-2">
+            <p className="font-poppins font-normal text-white text-[16px] leading-[30.8px]  ml-2">
               <span className="text-white">20%</span> Discount for{" "}
               <span className="text-white">1 Month</span> Account
             </p>
           </div>
 
-          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px">
+          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[56px] text-white ss:leading-[100.8px] leading-[75px">
             The Next <br className="sm:block hidden" />
             <span className="text-gradient ">Generation</span>
           </h1>
@@ -47,9 +47,9 @@ function App() {
             annual fees.
           </p>
         </div>
-        {/* <div className="flex-1 flex justify-center items-center  md:my-0 my-10 relative ">
-        <img src={robot} alt="billing" className="w-[80%] h-[80%] " />
-      </div> */}
+        <div className="flex-1 justify-end ">
+          <img src={robot} alt="billing" className="w-[80%] h-[80%] " />
+        </div>
       </div>
     </div>
   );
